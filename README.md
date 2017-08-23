58118d629aa19701ec8f1a84289813ed86ea9a8a
bangbang93 <bangbang93@163.com>
2017-08-23 11:57:14 +0800

    文档初步

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# getui-rest-sdk
+
+个推的nodejs sdk实在是太难用了……功能要啥啥没有，于是只好用rest-api重新封一遍
+
+## 接口参数参考
+<http://docs.getui.com/server/rest/push/>
+
+## Classes
+
+<dl>
+<dt><a href="#Getui">Getui</a> ⇐ <code>EventEmitter</code></dt>
+<dd><p>个推RestAPI SDK</p>
+</dd>
+</dl>
+
+## Typedefs
+
+<dl>
+<dt><a href="#<object>"><object></a></dt>
+<dd></dd>
+<dt><a href="#<object>"><object></a></dt>
+<dd></dd>
+</dl>
+
 <a name="Getui"></a>
 
 ## Getui ⇐ <code>EventEmitter</code>
@@ -10,6 +34,10 @@
     * [new Getui(appId, appKey, masterSecret)](#new_Getui_new)
     * [.waitAuth()](#Getui+waitAuth) ⇒ <code>Promise</code>
     * [.auth()](#Getui+auth) ⇒ <code>Promise.&lt;void&gt;</code>
+    * [.unauth()](#Getui+unauth) ⇒ <code>Promise.&lt;void&gt;</code>
+    * [.pushSingle(message, template, cid, apnsInfo, [requestId])](#Getui+pushSingle) ⇒ <code>Promise.&lt;object&gt;</code>
+    * [.pushSingleAlias(message, template, alias, apnsInfo, [requestId])](#Getui+pushSingleAlias) ⇒ <code>Promise.&lt;object&gt;</code>
+    * [.saveListBody(message, template, apnsInfo, taskName)](#Getui+saveListBody) ⇒ <code>Promise</code>
 
 <a name="new_Getui_new"></a>
 
@@ -35,3 +63,93 @@
 认证
 
 **Kind**: instance method of [<code>Getui</code>](#Getui)  
+**Throws**:
+
+- <code>Error</code> auth failed
+
+<a name="Getui+unauth"></a>
+
+### getui.unauth() ⇒ <code>Promise.&lt;void&gt;</code>
+关闭鉴权
+
+**Kind**: instance method of [<code>Getui</code>](#Getui)  
+<a name="Getui+pushSingle"></a>
+
+### getui.pushSingle(message, template, cid, apnsInfo, [requestId]) ⇒ <code>Promise.&lt;object&gt;</code>
+单推
+
+**Kind**: instance method of [<code>Getui</code>](#Getui)  
+**Returns**: <code>Promise.&lt;object&gt;</code> - 接口返回的json  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| message | <code>Message</code> | [Message](Message) |
+| template | <code>Notification</code> | [Notification](Notification) |
+| cid | <code>string</code> | cid |
+| apnsInfo | <code>string</code> | apns的json，ios需要 |
+| [requestId] | <code>string</code> | requestId |
+
+<a name="Getui+pushSingleAlias"></a>
+
+### getui.pushSingleAlias(message, template, alias, apnsInfo, [requestId]) ⇒ <code>Promise.&lt;object&gt;</code>
+单推给alias
+
+**Kind**: instance method of [<code>Getui</code>](#Getui)  
+**Returns**: <code>Promise.&lt;object&gt;</code> - 接口返回的json  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| message | <code>object</code> |  |
+| [message.appkey] | <code>string</code> | appkey 可忽略 |
+| message.is_offline | <code>boolean</code> | 是否离线消息 |
+| message.offline_expire_time | <code>number</code> | 离线消息保存时间(ms) |
+| message.msgtype | <code>string</code> | 消息类型 |
+| template | <code>object</code> |  |
+| template.style | <code>object</code> | 消息内容 |
+| template.transmission_type | <code>boolean</code> | 是否透传消息 |
+| template.transmission_content | <code>string</code> | 透传内容 |
+| alias | <code>string</code> | alias |
+| apnsInfo | <code>string</code> | apns的json，ios需要 |
+| [requestId] | <code>string</code> | requestId |
+
+<a name="Getui+saveListBody"></a>
+
+### getui.saveListBody(message, template, apnsInfo, taskName) ⇒ <code>Promise</code>
+保存消息共同体
+
+**Kind**: instance method of [<code>Getui</code>](#Getui)  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| message | <code>object</code> | [单推](单推) |
+| template |  |  |
+| apnsInfo |  |  |
+| taskName |  |  |
+
+<a name="<object>"></a>
+
+## <object>
+**Kind**: global typedef  
+**Properties**
+
+| Name | Type | Description |
+| --- | --- | --- |
+| is_offline | <code>boolean</code> | 是否存为离线消息 |
+| offline_expire_time | <code>number</code> | 离线时间 |
+| msgtype | <code>string</code> | 消息类型 |
+
+<a name="<object>"></a>
+
+## <object>
+**Kind**: global typedef  
+**Properties**
+
+| Name | Type | Description |
+| --- | --- | --- |
+| style | <code>object</code> | 样式 |
+| style.type | <code>number</code> | 样式类型，0-原生，1-个推样式，4-纯图，6-展开通知 |
+| style.text | <code>string</code> | text |
+| style.title | <code>string</code> | title |
+| transmission_type | <code>boolean</code> | 是否透传 |
+| transmission_content | <code>string</code> | 透传内容 |
+
--- index.js
+++ index.js
@@ -8,6 +8,23 @@ const sha256 = require('./helper/hash').sha256
 const randomstring = require('randomstring')
 
 /**
+ * @typedef <object> Message
+ * @property {boolean} is_offline 是否存为离线消息
+ * @property {number} offline_expire_time 离线时间
+ * @property {string} msgtype 消息类型
+ */
+
+/**
+ * @typedef <object> Notification
+ * @property {object} style 样式
+ * @property {number} style.type 样式类型，0-原生，1-个推样式，4-纯图，6-展开通知
+ * @property {string} style.text text
+ * @property {string} style.title title
+ * @property {boolean} transmission_type 是否透传
+ * @property {string} transmission_content 透传内容
+ */
+
+/**
  * 个推RestAPI SDK
  *
  * @extends EventEmitter
@@ -40,6 +57,7 @@ class Getui extends EventEmitter {
 
   /**
    * 认证
+   * @throws {Error} auth failed
    * @returns {Promise.<void>}
    */
   async auth() {
@@ -63,6 +81,10 @@ class Getui extends EventEmitter {
     this.emit('ready')
   }
 
+  /**
+   * 关闭鉴权
+   * @returns {Promise.<void>}
+   */
   async unauth() {
     const res = await this._http(`https://restapi.getui.com/v1/${this._appId}/auth_close`)
     if (res.result !== 'ok') {
@@ -70,7 +92,17 @@ class Getui extends EventEmitter {
     }
   }
 
+  /**
+   * 单推
+   * @param {Message} message {@link Message}
+   * @param {Notification} template {@link Notification}
+   * @param {string} cid cid
+   * @param {string} apnsInfo apns的json，ios需要
+   * @param {string} [requestId] requestId
+   * @returns {Promise<object>} 接口返回的json
+   */
   pushSingle(message, template, cid, apnsInfo, requestId = getRequestId()) {
+    message.appkey = message.appkey || this._appKey
     if (typeof apnsInfo === 'object') {
       apnsInfo = JSON.stringify(apnsInfo);
     }
@@ -84,6 +116,38 @@ class Getui extends EventEmitter {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/push_single`, body);
   }
 
+  /**
+   * 单推给alias
+   * @param {Message} message {@link Message}
+   * @param {Notification} template {@link Notification}
+   * @param {string} alias alias
+   * @param {string} apnsInfo apns的json，ios需要
+   * @param {string} [requestId] requestId
+   * @returns {Promise<object>} 接口返回的json
+   */
+  pushSingleAlias(message, template, alias, apnsInfo, requestId = getRequestId()) {
+    message.appkey = message.appkey || this._appKey
+    if (typeof apnsInfo === 'object') {
+      apnsInfo = JSON.stringify(apnsInfo);
+    }
+    const body = {
+      message,
+      alias,
+      push_info: apnsInfo,
+      requestid: requestId
+    }
+    body[message['msgtype']] = template
+    return this._http(`https://restapi.getui.com/v1/${this._appId}/push_single`, body);
+  }
+
+  /**
+   * 保存消息共同体
+   * @param {Message} message {@link Message}
+   * @param {Notification} template {@link Notification}
+   * @param {string} apnsInfo apns的json，ios需要
+   * @param {string} [taskName] 任务名称
+   * @returns {Promise}
+   */
   saveListBody(message, template, apnsInfo, taskName) {
     const body = {
       message,
@@ -94,7 +158,14 @@ class Getui extends EventEmitter {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/save_list_body`);
   }
 
-  pushList(cidList, taskId, needDetail) {
+  /**
+   * 群推
+   * @param {array} cidList cidList
+   * @param {string} taskId 保存消息共同体的返回结果
+   * @param {boolean} [needDetail] 默认值:false，是否需要返回每个CID的状态
+   * @returns {Promise}
+   */
+  toList(cidList, taskId, needDetail) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/push_list`, {
       cid        : cidList,
       taskid     : taskId,
@@ -102,6 +173,30 @@ class Getui extends EventEmitter {
     });
   }
 
+  /**
+   * 群推alias
+   * @param {array} aliasList alias
+   * @param {string} taskId 保存消息共同体的返回结果
+   * @param {boolean} [needDetail] 默认值:false，是否需要返回每个CID的状态
+   * @returns {Promise}
+   */
+  toListAlias(aliasList, taskId, needDetail) {
+    return this._http(`https://restapi.getui.com/v1/${this._appId}/push_list`, {
+      alias      : aliasList,
+      taskid     : taskId,
+      need_detail: needDetail,
+    });
+  }
+
+  /**
+   * toapp群推
+   * @param {Message} message {@link Message}
+   * @param {Notification} template {@link Notification}
+   * @param {string} apnsInfo
+   * @param {object} [condition] 筛选目标用户条件，参考下面的condition说明
+   * @param {string} [requestId]
+   * @returns {Promise}
+   */
   pushApp(message, template, apnsInfo, condition, requestId = getRequestId()) {
     const body = {
       message,
@@ -113,10 +208,20 @@ class Getui extends EventEmitter {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/push_app`, body)
   }
 
+  /**
+   * 停止群推任务
+   * @param {string} taskId
+   * @returns {Promise}
+   */
   stopTask(taskId) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/stop_task/${taskId}`, null, 'delete')
   }
 
+  /**
+   * 批量单推
+   * @param {array<object>} msgList
+   * @returns {Promise}
+   */
   pushSingleBatch(msgList) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/push_single_batch`, {
       msg_list: msgList,
@@ -124,20 +229,43 @@ class Getui extends EventEmitter {
 
   }
 
+  /**
+   * 绑定别名
+   * @param {array<object>} aliasList[]
+   * @param {string} aliasList[].cid cid
+   * @param {string} aliasList[].alias 别名
+   * @returns {Promise}
+   */
   bindAlias(aliasList) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/bind_alias`, {
       alias_list: aliasList,
     })
   }
 
+  /**
+   * 别名查询
+   * @param {string} alias
+   * @returns {Promise}
+   */
   queryCid(alias) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/query_cid/${alias}`, null, 'get')
   }
 
+  /**
+   * 根据cid查询别名
+   * @param {string} cid
+   * @returns {Promise}
+   */
   queryAlias(cid) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/query_alias/${cid}`, null, 'get')
   }
 
+  /**
+   * 单个cid和别名解绑
+   * @param {string} cid
+   * @param {string} alias
+   * @returns {Promise}
+   */
   unbindAlias(cid, alias) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/unbind_alias`, {
       cid,
@@ -145,12 +273,23 @@ class Getui extends EventEmitter {
     })
   }
 
+  /**
+   * 解绑别名所有cid
+   * @param {string} alias
+   * @returns {Promise}
+   */
   unbindAliasAll(alias) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/unbind_alias_all`, {
       alias
     })
   }
 
+  /**
+   * 设置tag
+   * @param {string} cid
+   * @param {array<string>} tagList
+   * @returns {Promise}
+   */
   setTags(cid, tagList) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/set_tags`, {
       cid,
@@ -158,40 +297,81 @@ class Getui extends EventEmitter {
     })
   }
 
+  /**
+   * 获取cid的tag
+   * @param {string} cid
+   * @returns {Promise}
+   */
   getTags(cid) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/get_tags/${cid}`, null, 'get')
   }
 
+  /**
+   * 添加用户黑名单
+   * @param {string} cid
+   * @returns {Promise}
+   */
   userBlkListAdd(cid) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/user_blk_list`, {
       cid
     })
   }
 
+  /**
+   * 删除用户黑名单
+   * @param {string} cid
+   * @returns {Promise}
+   */
   userBlkListDelete(cid) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/user_blk_list`, {
       cid
     }, 'delete')
   }
 
+  /**
+   * 查询用户状态
+   * @param {string} cid
+   * @returns {Promise}
+   */
   userStatus(cid) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/user_status/${cid}`, null, 'get')
   }
 
+  /**
+   * 获取推送结果
+   * @param {array<string>} taskIdList
+   * @returns {Promise}
+   */
   pushResult(taskIdList) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/push_result`, {
       taskidlist: taskIdList,
     })
   }
 
+  /**
+   * 获取单日用户数据
+   * @param {string} date yyyymmdd 20170823
+   * @returns {Promise}
+   */
   queryAppUser(date) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/query_app_user/${date}`, null, 'get')
   }
 
+  /**
+   * 获取单日推送数据
+   * @param {string} date yyyymmdd 20170823
+   * @returns {Promise}
+   */
   queryAppPush(date) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/query_app_push/${date}`, null, 'get')
   }
 
+  /**
+   * 应用角标设置接口(仅iOS)
+   * @param {number} badge
+   * @param {array<string>} cids
+   * @returns {Promise}
+   */
   setBadge(badge, cids) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/set_badge`, {
       badge,
@@ -199,24 +379,48 @@ class Getui extends EventEmitter {
     })
   }
 
+  /**
+   * 根据任务组名获取推送结果
+   * @param {string} groupName
+   * @returns {Promise}
+   */
   getPushResultByGroupName(groupName) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/get_push_result_by_group_name/${groupName}`, null, 'get')
   }
 
+  /**
+   * 获取24小时在线用户数
+   * @returns {Promise}
+   */
   getLast24HoursOnlineUsreStatistics() {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/get_last_24hours_online_User_statistics`, null, 'post')
   }
 
+  /**
+   * 按条件查询用户数
+   * @param {object} condition
+   * @returns {Promise}
+   */
   queryUserCount(condition) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/query_user_count`, {
       condition
     }, 'get')
   }
 
+  /**
+   * 获取可用bi标签
+   * @returns {Promise}
+   */
   queryBiTags() {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/query_bi_tags`, null, 'get')
   }
 
+  /**
+   * 获取回执的用户列表
+   * @param {string} taskId
+   * @param {string} cids
+   * @returns {Promise}
+   */
   getFeedbackUsers(taskId, cids) {
     return this._http(`https://restapi.getui.com/v1/${this._appId}/get_feedback_users`, {
       taskId,
